1eeb703cf74e72685fda3ee57f5742962b6d434a
test <test>
2020-08-31 09:53:17 +0800

    fixed bug

--- delete.sh
+++ delete.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 path=$1
-if [ -f "${path}/test_flow1.json" ];then
+if [ -f "test_flow1.json" ];then
 	curl -u onos:rocks -X DELETE --header 'Accept: application/json' 'http://localhost:8181/onos/v1/flows/application/app'
 fi
 for (( i=5 ; i>=1 ; i=i-1))
 do
-	if [ -f "${path}/test_group$i.json" ];then
+	if [ -f "test_group$i.json" ];then
 		curl -u onos:rocks -X DELETE --header 'Accept: application/json' "http://localhost:8181/onos/v1/groups/of%3A000078321bdf7600/0x${i}234abc3"
 	fi
 done
--- upload.sh
+++ upload.sh
@@ -6,8 +6,8 @@ up_l_m1="'http://localhost:8181/onos/v1/meters/of:000078321bdf7600'"
 path=$1
 for (( i=1; i<=5; i=i+1 ))
 do
-	if [ -f "${path}/test_group$i.json" ];then
-		comd="$up_l @${path}/test_group$i.json $up_l_g1"
+	if [ -f "test_group$i.json" ];then
+		comd="$up_l @test_group$i.json $up_l_g1"
 		eval "$comd"
 	fi
 done
@@ -20,8 +20,8 @@ fi
 done
 for (( i=1; i<=5; i=i+1 ))
 do
-if [ -f "${path}/test_flow$i.json" ];then
-	comd="$up_l @${path}/test_flow$i.json $up_l_f1"	
+if [ -f "test_flow$i.json" ];then
+	comd="$up_l @test_flow$i.json $up_l_f1"	
 	eval "$comd"
 fi
 done
